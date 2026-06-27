9d5c7ea57934029d54972a0d357bc507117e148e
Shivam Kumar Chaubey <shivamc36@gmail.com>
2026-06-27 11:58:25 +0530

    Revert "Log OTP to server console in dev for local testing"
    This reverts commit 12a36e22274c49d7ff57409945ee8c2c96ddf444.

--- apps/accounts/tasks.py
+++ apps/accounts/tasks.py
@@ -12,7 +12,6 @@ logger = logging.getLogger(__name__)
 def send_otp_sms(phone, code):
     """Send OTP via SMS. Currently logs to console; plug in SMS provider (Twilio/MSG91) here."""
     logger.info("[SMS] OTP %s sent to %s", code, phone)
-    print("The otp is ", code)
     return {"phone": phone, "status": "sent"}
 
 
